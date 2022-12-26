39bd5ac00bd09e53689a59d486a41165da14d770
samdungedup <samdungedup@gmail.com>
2022-12-26 11:20:15 +0600

    cards list

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,9 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all.order("created_at DESC")
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
 end
